992e7069496d8087f7af697b210d488deb23ebb3
Felix Packard <felix@passle.net>
2022-05-17 13:02:28 +0100

    Update ControllerBase to use permission_callback rather than validate_callback, so that 401 errors are returned rather than 400

--- class/Controllers/ControllerBase.php
+++ class/Controllers/ControllerBase.php
@@ -3,6 +3,7 @@
 namespace Passle\PassleSync\Controllers;
 
 use Passle\PassleSync\Services\OptionsService;
+use \WP_REST_Request;
 
 abstract class ControllerBase
 {
@@ -17,22 +18,22 @@ abstract class ControllerBase
 
   public abstract function register_routes();
 
-  protected function register_route(string $path, string $method, string $func_name, string $validate_callback = "validate_admin_dashboard_request")
+  protected function register_route(string $path, string $method, string $func_name, string $permission_callback = "validate_admin_dashboard_request")
   {
     register_rest_route(PASSLESYNC_REST_API_BASE, $path, [
       "methods" => $method,
       "callback" => [$this, $func_name],
-      "validate_callback" => [$this, $validate_callback],
-      "permission_callback" => "__return_true",
+      "validate_callback" => "__return_true",
+      "permission_callback" => [$this, $permission_callback],
     ]);
   }
 
-  public function validate_admin_dashboard_request($request): bool
+  public function validate_admin_dashboard_request(): bool
   {
     return current_user_can("administrator");
   }
 
-  public function validate_passle_webhook_request($request): bool
+  public function validate_passle_webhook_request(WP_REST_Request $request): bool
   {
     return $request->get_header("APIKey") === $this->plugin_api_key;
   }
