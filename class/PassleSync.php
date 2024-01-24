9928aa88586d7fac16b5f4a82c201d2d5bbb18e9
Theodoros Klikas <theodoros@passle.net>
2024-01-24 14:31:44 +0000

    Added caching mechanism to tag groups so that we don't call the API every time to create them as taxonomies. Also a cron job creation that can be used to clear tag groups plugin cache when tag groups change on passle as they are not really sync-ing using the traditional way at the moment

--- class/PassleSync.php
+++ class/PassleSync.php
@@ -15,8 +15,12 @@ use Passle\PassleSync\Services\TemplateService;
 use Passle\PassleSync\Services\ThemeService;
 use Passle\PassleSync\Services\UpgradeService;
 
+use Passle\PassleSync\Services\Content\Passle\PassleTagGroupsContentService;
+
 class PassleSync
 {
+  private const TAG_GROUPS_CACHE_CLEAN_EVENT_NAME = "passle_sync_clear_tag_groups_cache_event";
+
   public static function initialize()
   {
     MenuService::init();
@@ -32,17 +36,45 @@ class PassleSync
     TemplateService::init();
     UpgradeService::init();
 
-    register_activation_hook(__FILE__, [static::class, "activate"]);
-    register_deactivation_hook(__FILE__, [static::class, "deactivate"]);
+    add_action(self::TAG_GROUPS_CACHE_CLEAN_EVENT_NAME, [static::class, "tag_groups_cache_cleanup"]);
   }
 
   public static function activate()
   {
     flush_rewrite_rules();
+    self::schedule_tag_groups_cache_cleanup();
   }
 
   public static function deactivate()
   {
     flush_rewrite_rules();
+    self::unschedule_tag_groups_cache_cleanup();
+  }
+
+  /*
+  *  The following code deals with scheduling tag groups cache cleanup, so when a new
+  *  tag group is created in Passle, at some point within the hour, it is also created in WP
+  *  following the init event. 
+  *  If a Resource class is created for passle tag groups, this code will need to be moved probably.
+  */ 
+  public static function schedule_tag_groups_cache_cleanup() 
+  {
+    if (!wp_next_scheduled(self::TAG_GROUPS_CACHE_CLEAN_EVENT_NAME)) {
+      wp_schedule_event(time(), "hourly", self::TAG_GROUPS_CACHE_CLEAN_EVENT_NAME);
+    }
+  }
+
+  public static function tag_groups_cache_cleanup() 
+  {
+     PassleTagGroupsContentService::overwrite_cache(array());
+  }
+
+  public static function unschedule_tag_groups_cache_cleanup()
+  {
+    $timestamp = wp_next_scheduled(self::TAG_GROUPS_CACHE_CLEAN_EVENT_NAME);
+    if ($timestamp) {
+        wp_unschedule_event($timestamp, self::TAG_GROUPS_CACHE_CLEAN_EVENT_NAME);
+    }
   }
 }
+
