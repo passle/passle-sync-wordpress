a9938b388f0654403975153758e9dcaefa03dd0c
oscar-welton <oscar@passle.net>
2024-02-01 13:07:48 +0000

    fix - reverted some changes and fixed the early return error

--- class/Actions/UpdateFeaturedPostAction.php
+++ class/Actions/UpdateFeaturedPostAction.php
@@ -3,12 +3,16 @@
 namespace Passle\PassleSync\Actions;
 
 use Passle\PassleSync\Utils\Utils;
+use WP_Error;
 
 class UpdateFeaturedPostAction
 {
   public static function execute(string $post_shortcode, bool $is_featured_on_passle_page, bool $is_featured_on_post_page)
   {
     Utils::clear_featured_posts();
+
+    // if the shortcode is empty it means the featured post has been removed and therefore no need to continue. 
+    if (empty($post_shortcode)) return; 
 
     $posts = get_posts([
       "post_type" => PASSLESYNC_POST_TYPE,
@@ -20,16 +24,17 @@ class UpdateFeaturedPostAction
         ],
       ],
     ]);
-    
-    if (!empty($posts)) {
-      $post = $posts[0];
-      if ($is_featured_on_passle_page) {
-        update_post_meta($post->ID, "post_is_featured_on_passle_page", true);
-      }
-  
-      if ($is_featured_on_post_page) {
-        update_post_meta($post->ID, "post_is_featured_on_post_page", true);
-      }
+
+    if (empty($posts)) return new WP_Error("404", "No post exists with the shortcode specified");
+    $post = $posts[0];
+
+
+    if ($is_featured_on_passle_page) {
+      update_post_meta($post->ID, "post_is_featured_on_passle_page", true);
+    }
+
+    if ($is_featured_on_post_page) {
+      update_post_meta($post->ID, "post_is_featured_on_post_page", true);
     }
   }
 }
